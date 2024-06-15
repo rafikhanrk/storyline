f1a118e63039a82b84209a43327aec256ee5f96e
rafikhanrk <bythakhan@gmail.com>
2024-06-15 14:41:49 -0700

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault();
+            const targetId = this.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+            targetSection.scrollIntoView({ behavior: 'smooth' });
+        });
+    });
+});
